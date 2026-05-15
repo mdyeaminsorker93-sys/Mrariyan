a71d1e434ae70e57de6bd3f9da591ec999bebe97
mdyeaminsorker93-sys <mdyeaminsorker93@gmail.com>
2026-05-15 13:13:26 +0600

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,3 @@
-console.log("Workflow Running Successfully");
+console.log("Hello, Ariyan! Your Node.js app is running successfully on GitHub Actions.");
+
+// যদি কোনো বিশেষ কোড থাকে তা এখানে যোগ করতে পারেন।
